a0f94077a54847bb2bd8a2a8c63604cc7ee10727
Corniel Nobel <corniel@gmail.com>
2023-07-06 21:04:57 +0200

    New Rule Proj0005: Define package reference assets as attributes (#7)

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/Define_package_reference_assets_as_attributes.cs
@@ -0,0 +1,23 @@
+﻿namespace Rules.Define_package_reference_assets_as_attributes;
+
+public class Reports
+{
+    [Test]
+    public void assets_as_elements()
+        => new DefinePackageReferenceAssetsAsAttributes()
+        .ForProject("PackageReferenceAssetsAsElements.cs")
+        .HasIssues(
+            new Issue("Proj0005", "Define package reference assets of 'BackwardsCompatibleFeatures' as attributes.").WithSpan(8, 5, 10, 23),
+            new Issue("Proj0005", "Define package reference assets of 'SonarAnalyzer.CSharp' as attributes.").WithSpan(11, 5, 14, 23));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantVB.vb")]
+    [TestCase("PackageReferenceAssetsAsAttributes.cs")]
+    public void assets_as_attributes_or_without(string project)
+         => new DefinePackageReferenceAssetsAsAttributes()
+        .ForProject(project)
+        .HasNoIssues();
+}
