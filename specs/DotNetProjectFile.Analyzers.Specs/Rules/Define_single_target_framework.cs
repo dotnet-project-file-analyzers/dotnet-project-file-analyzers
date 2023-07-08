c7d8c4fa5e771c60af043f27c90e89aa4bed67dc
Corniel Nobel <corniel@gmail.com>
2023-07-08 17:23:09 +0200

    Implement Proj0009: Use the TragetFramework node for a single target framework.

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/Define_single_target_framework.cs
@@ -0,0 +1,22 @@
+﻿namespace Specs.Rules.Define_single_target_framework;
+
+public class Reports
+{
+    [Test]
+    public void defined_in_traget_frameworks()
+        => new DefineSingleTargetFramework()
+        .ForProject("TargetFrameworksSingle.cs")
+        .HasIssues(
+            new Issue("Proj0009", "Use the TargetFramework node instead.").WithSpan(3, 5, 3, 47));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantVB.vb")]
+    [TestCase("TargetFrameworksMultiple.cs")]
+    public void Projects_with_analyzers(string project)
+         => new DefineSingleTargetFramework()
+        .ForProject(project)
+        .HasNoIssues();
+}
--- src/DotNetProjectFile.Analyzers/Rule.cs
+++ src/DotNetProjectFile.Analyzers/Rule.cs
@@ -1,5 +1,7 @@
 ﻿#pragma warning disable SA1118 // Parameter should not span multiple lines: readability for descriptions.
 
+using DotNetProjectFile.Xml;
+
 namespace DotNetProjectFile;
 
 public static class Rule
@@ -83,16 +85,28 @@ public static class Rule
         isEnabled: true);
 
     public static DiagnosticDescriptor RemoveFolderNodes => New(
-       id: 0008,
-       title: "Remove folder nodes",
-       message: "Remove folder node '{0}'.",
-       description:
-        "Folders nodes only add noise. They are leftovers of directories " +
-        "created in the IDE, without adding an actual file to it.",
-       tags: new[] { "noise" },
-       category: Category.Clarity,
-       severity: DiagnosticSeverity.Warning,
-       isEnabled: true);
+        id: 0008,
+        title: "Remove folder nodes",
+        message: "Remove folder node '{0}'.",
+        description:
+            "Folders nodes only add noise. They are leftovers of directories " +
+            "created in the IDE, without adding an actual file to it.",
+        tags: new[] { "noise" },
+        category: Category.Clarity,
+        severity: DiagnosticSeverity.Warning,
+        isEnabled: true);
+
+    public static DiagnosticDescriptor DefineSingleTargetFramework => New(
+        id: 0009,
+        title: "Use the TragetFramework node for a single target framework",
+        message: "Use the TargetFramework node instead.",
+        description:
+            "To prevent confusion, only use the TargetFrameworks node when " +
+            "there are multiple target frameworks.",
+        tags: new[] { "target framework", "confusion" },
+        category: Category.Clarity,
+        severity: DiagnosticSeverity.Warning,
+        isEnabled: true);
 
     public static DiagnosticDescriptor UseDotNetProjectFileAnalyzers => New(
         id: 1000,
