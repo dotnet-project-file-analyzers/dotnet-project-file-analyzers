dacb96fdf13b12bb3cb276a8f747566552ee2888
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2023-07-11 22:01:24 +0200

    Fix typos

--- src/DotNetProjectFile.Analyzers/Rule.cs
+++ src/DotNetProjectFile.Analyzers/Rule.cs
@@ -96,7 +96,7 @@ public static class Rule
 
     public static DiagnosticDescriptor DefineSingleTargetFramework => New(
         id: 0009,
-        title: "Use the TragetFramework node for a single target framework",
+        title: "Use the TargetFramework node for a single target framework",
         message: "Use the TargetFramework node instead.",
         description:
             "To prevent confusion, only use the TargetFrameworks node when " +
@@ -141,7 +141,7 @@ public static class Rule
         id: 2002,
         title: "Sort resource file values alphabetically",
         message: "Resource values should be ordered alphabetically by their names.",
-        description: 
+        description:
             "To improve readability, and reduce the number of merge conflicts, " +
             "the `<data>` elements should be sorted based on the `@name` attribute.",
         tags: new[] { "resx", "resources" },
