65846d153265d0c5115f4aa50ee59abf333f2129
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2023-07-15 20:44:43 +0200

    Add rule for deprecated node

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/AvoidLicenseUrl.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class AvoidLicenseUrl : MsBuildProjectFileAnalyzer
+{
+    public AvoidLicenseUrl() : base(Rule.AvoidLicenseUrl) { }
+
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        if (context.Project.IsProject)
+        {
+            var nodes = context.Project
+                .ImportsAndSelf()
+                .SelectMany(p => p.PropertyGroups)
+                .SelectMany(g => g.PackageLicenseUrl);
+
+            if (nodes.Any())
+            {
+                context.ReportDiagnostic(Descriptor, context.Project);
+            }
+        }
+    }
+}
