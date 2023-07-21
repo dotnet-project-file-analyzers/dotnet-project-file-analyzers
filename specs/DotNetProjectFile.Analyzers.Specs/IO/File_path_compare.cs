0be8314cb1a54abe2a54547d5ca2bfcde63e6f8b
Corniel Nobel <corniel@gmail.com>
2023-07-21 12:21:43 +0200

    Sort file paths.

---
+++ specs/DotNetProjectFile.Analyzers.Specs/IO/File_path_compare.cs
@@ -0,0 +1,38 @@
+﻿using DotNetProjectFile.IO;
+
+namespace IO.File_path_compare;
+
+public class Compares
+{
+    [TestCase("/path/", "\\path\\")]
+    [TestCase("/path/", "/path\\")]
+    [TestCase("/path/", "/path/")]
+    public void Same_paths_as_equal(string left, string right)
+        => FileSystem.PathCompare.Compare(left, right).Should().Be(0);
+
+    [Test]
+    public void sub_strings_before()
+        => new[] 
+        {
+            "substring",
+            "sub",
+        }
+        .OrderBy(x => x, FileSystem.PathCompare)
+        .Should().BeEquivalentTo(
+            "sub",
+            "substring"
+        );
+
+    [Test]
+    public void shorter_paths_first()
+        => new[] 
+        {
+            @"..\Root.AspNetCore.Builder.Abstractions\Root.AspNetCore.Builder.Abstractions.csproj",
+            @"..\Root.AspNetCore\Root.AspNetCore.csproj",
+        }
+        .OrderBy(x => x, FileSystem.PathCompare)
+        .Should().BeEquivalentTo(
+            @"..\Root.AspNetCore\Root.AspNetCore.csproj",
+            @"..\Root.AspNetCore.Builder.Abstractions\Root.AspNetCore.Builder.Abstractions.csproj"
+        );
+}
---
+++ src/DotNetProjectFile.Analyzers/IO/FilePathComparer.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNetProjectFile.IO;
+
+public sealed class FilePathComparer : IComparer<string>
+{
+    /// <inheritdoc />
+    public int Compare(string x, string y)
+    {
+        int? compare = null;
+        var xs = x.GetEnumerator();
+        var ys = y.GetEnumerator();
+
+        while (compare is null && xs.MoveNext() && ys.MoveNext())
+        {
+            compare = Compare(xs.Current, ys.Current);
+        }
+        return compare ?? y.Length.CompareTo(x.Length);
+    }
+
+    private int? Compare(char x, char y)
+    {
+        int compare = Update(x).CompareTo(Update(y));
+        return compare == 0 ? null : compare;
+
+        static char Update(char ch) => ch == '/' || ch == '\\' ? char.MinValue : ch;
+    }
+}
