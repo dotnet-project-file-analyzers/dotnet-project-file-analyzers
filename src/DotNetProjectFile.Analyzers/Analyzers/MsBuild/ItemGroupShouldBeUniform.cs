8254ca506baf78de4f5f0da92e9cc3216efa97c8
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2023-08-02 23:02:05 +0200

    Add analyzer and test project

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/ItemGroupShouldBeUniform.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class ItemGroupShouldBeUniform : MsBuildProjectFileAnalyzer
+{
+    public ItemGroupShouldBeUniform() : base(Rule.ItemGroupShouldBeUniform) { }
+
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        foreach (var group in context.Project
+            .ImportsAndSelf()
+            .SelectMany(p => p.ItemGroups))
+        {
+            AnalyzeGroup(context, group);
+        }
+    }
+
+    private void AnalyzeGroup(ProjectFileAnalysisContext context, ItemGroup group)
+    {
+        if (group.Children.FirstOrDefault()?.LocalName is not { } type)
+        {
+            return;
+        }
+
+        if (group.Children.Any(n => n.LocalName != type))
+        {
+            context.ReportDiagnostic(Descriptor, group);
+        }
+    }
+}
