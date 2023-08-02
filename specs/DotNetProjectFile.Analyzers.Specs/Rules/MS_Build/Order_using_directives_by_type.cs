1dc023f117bcdb836ef5501e5e25e4ee27886184
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2023-08-02 22:05:45 +0200

    Apply suggestion

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Order_using_directives_by_type.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Order_using_directives_by_type.cs
@@ -8,12 +8,12 @@ public class Reports
        .ForProject("IncorrectlyOrderedUsingDirectives.cs")
        .HasIssues(
            // First set are compiler errors that can be ignored for the sake of testing this analyzer.
-           new Issue("CS0400", @"The type or namespace name 'NamespaceA' could not be found in the global namespace (are you missing an assembly reference?)").WithSpan(3, 36, 3, 46).WithSeverity(DiagnosticSeverity.Error),
-           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)").WithSpan(5, 34, 5, 44).WithSeverity(DiagnosticSeverity.Error),
-           new Issue("CS0400", @"The type or namespace name 'NamespaceA' could not be found in the global namespace (are you missing an assembly reference?)").WithSpan(1, 21, 1, 31).WithSeverity(DiagnosticSeverity.Error),
-           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)").WithSpan(2, 21, 2, 31).WithSeverity(DiagnosticSeverity.Error),
-           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)").WithSpan(4, 28, 4, 38).WithSeverity(DiagnosticSeverity.Error),
-           new Issue("CS8085", @"A 'using static' directive cannot be used to declare an alias").WithSpan(5, 20, 5, 23).WithSeverity(DiagnosticSeverity.Error),
+           new Issue("CS0400", @"The type or namespace name 'NamespaceA' could not be found in the global namespace (are you missing an assembly reference?)", DiagnosticSeverity.Error).WithSpan(3, 36, 3, 46),
+           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)", DiagnosticSeverity.Error).WithSpan(5, 34, 5, 44),
+           new Issue("CS0400", @"The type or namespace name 'NamespaceA' could not be found in the global namespace (are you missing an assembly reference?)", DiagnosticSeverity.Error).WithSpan(1, 21, 1, 31),
+           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)", DiagnosticSeverity.Error).WithSpan(2, 21, 2, 31),
+           new Issue("CS0400", @"The type or namespace name 'NamespaceB' could not be found in the global namespace (are you missing an assembly reference?)", DiagnosticSeverity.Error).WithSpan(4, 28, 4, 38),
+           new Issue("CS8085", @"A 'using static' directive cannot be used to declare an alias", DiagnosticSeverity.Error).WithSpan(5, 20, 5, 23),
 
            new Issue("Proj0018", @"Using directive for 'NamespaceB' is should appear before Using directive for 'NamespaceB'.").WithSpan(7, 5, 7, 34));
 }
