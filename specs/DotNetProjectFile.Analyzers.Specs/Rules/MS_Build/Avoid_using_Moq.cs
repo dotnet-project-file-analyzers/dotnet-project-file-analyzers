e5f2b3811105b369c2b9dbe4a1c41af4d3d1b788
Corniel Nobel <corniel@gmail.com>
2023-08-09 15:27:41 +0200

    Do not use Moq. Period.

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Avoid_using_Moq.cs
@@ -0,0 +1,22 @@
+﻿namespace Specs.Rules.MS_Build.Avoid_using_Moq;
+
+public class Reports
+{
+    [Test]
+    public void on_Moq_dependency()
+       => new AvoidUsingMoq()
+       .ForProject("UsesMoq.cs")
+       .HasIssue(
+            new Issue("Proj1100", " Do not use Moq.")
+                .WithSpan(29, 5, 9, 55));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void Projects_without_issues(string project)
+         => new AvoidUsingMoq()
+        .ForProject(project)
+        .HasNoIssues();
+}
---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/AvoidUsingMoq.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class AvoidUsingMoq : MsBuildProjectFileAnalyzer
+{
+    public AvoidUsingMoq() : base(Rule.AvoidUsingMoq) { }
+
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        foreach (var reference in context.Project.ItemGroups
+            .SelectMany(i => i.PackageReferences)
+            .Where(p => p.Include == "Moq"))
+        {
+            context.ReportDiagnostic(Descriptor, reference);
+        }
+    }
+}
