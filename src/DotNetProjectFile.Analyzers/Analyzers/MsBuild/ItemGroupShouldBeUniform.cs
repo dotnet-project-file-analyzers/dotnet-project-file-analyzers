3328e9920ab44840f054ada73ba1aa4d9da00c6f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-02-20 20:14:48 +0100

    no message

--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/ItemGroupShouldBeUniform.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/ItemGroupShouldBeUniform.cs
@@ -3,6 +3,19 @@
 [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
 public sealed class ItemGroupShouldBeUniform : MsBuildProjectFileAnalyzer
 {
+    private static readonly IReadOnlyCollection<IReadOnlyCollection<string>> Exceptions =
+        [
+            [
+                "Compile",
+                "EmbeddedResource",
+                "Content",
+                "None",
+            ],
+        ];
+
+    private static readonly IReadOnlyDictionary<string, IReadOnlyCollection<string>> Allowed
+        = GenerateAllowList();
+
     public ItemGroupShouldBeUniform() : base(Rule.ItemGroupShouldBeUniform) { }
 
     protected override void Register(ProjectFileAnalysisContext context)
@@ -22,9 +35,41 @@ public sealed class ItemGroupShouldBeUniform : MsBuildProjectFileAnalyzer
             return;
         }
 
-        if (group.Children.Any(n => n.LocalName != type))
+        if (group.Children.Any(n => !CombinationIsAllowed(type, n.LocalName)))
         {
             context.ReportDiagnostic(Descriptor, group);
         }
+    }
+
+    private static bool CombinationIsAllowed(string first, string second)
+    {
+        if (first == second)
+        {
+            return true;
+        }
+        else if (Allowed.TryGetValue(first, out var allowed))
+        {
+            return allowed.Contains(second);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static IReadOnlyDictionary<string, IReadOnlyCollection<string>> GenerateAllowList()
+    {
+        var result = new Dictionary<string, IReadOnlyCollection<string>>();
+
+        foreach (var list in Exceptions)
+        {
+            var set = new HashSet<string>(list);
+            foreach (var item in set)
+            {
+                result[item] = set;
+            }
+        }
+
+        return result;
     }
 }
