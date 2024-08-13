aae67b24699670a7dea4a61fe00f7307cc3340f9
Corniel Nobel <corniel@gmail.com>
2024-08-13 20:18:06 +0200

    Support the suppression of rules (in XML based) files.

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Pragma_warning_disable.cs
@@ -0,0 +1,10 @@
+﻿namespace Rules.MS_Build.Pragma_warning_disable;
+
+public class Guard
+{
+    [Test]
+    public void disabled()
+        => new RemoveFolderNodes()
+        .ForProject("SuppressIssues.cs")
+        .HasIssue(new Issue("Proj0008", @"Remove folder node 'Third'").WithSpan(19, 05, 19, 30));
+}
