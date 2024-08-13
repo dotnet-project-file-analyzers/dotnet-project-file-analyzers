3e9e1b88273efb21fde8f3dbd116836047de8485
Corniel Nobel <corniel@gmail.com>
2024-08-13 22:48:04 +0200

    FIrst implementation pragma warning (parsing).

---
+++ specs/DotNetProjectFile.Analyzers.Specs/MS_Build/Pragma_warning.cs
@@ -0,0 +1,20 @@
+﻿using DotNetProjectFile.MsBuild;
+
+namespace MS_Build.Pragma_warning;
+
+public class Parses
+{
+    [TestCase("#pragma warning disable Proj0007")]
+    [TestCase("#pragma warning disable Proj0007 ")]
+    [TestCase("#pragma warning disable Proj0007\n")]
+    [TestCase("#pragma warning disable Proj0007\t")]
+    [TestCase("#pragma warning disable Proj0007\r\n")]
+    [TestCase("#pragma warning disable Proj0007 with some ignored text afterwards")]
+    [TestCase("  #pragma   warning   disable   Proj0007")]
+    public void Disable(string str)
+        => PragmaWarning.TryParse(str).Should().Be(new PragmaWarning("Project0007", true));
+
+    [TestCase("#pragma warning restore Proj0007")]
+    public void Restore(string str)
+        => PragmaWarning.TryParse(str).Should().Be(new PragmaWarning("Project0007", false));
+}
---
+++ src/DotNetProjectFile.Analyzers/MsBuild/PragmaWarning.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DotNetProjectFile.MsBuild;
+
+public readonly struct PragmaWarning(string diagnosticId, bool disable)
+{
+    public string DiagnosticId { get; } = diagnosticId;
+
+    public bool Disable { get; } = disable;
+
+    public override string ToString()
+        => Disable
+        ? $"#pragma warning disable {DiagnosticId}"
+        : $"#pragma resore disable {DiagnosticId}";
+
+    public static PragmaWarning? TryParse(string? str)
+        => str is { }
+        && Pattern.Match(str) is { Success: true } match
+            ? new(
+                match.Groups[nameof(DiagnosticId)].Value,
+                match.Groups[nameof(Disable)].Value == "disable")
+            : null;
+
+    private static readonly Regex Pattern = new(
+        @"^ *#pragma +warning +(?<Disable>disable|restore) +(?<DiagnosticId>\w+)(\s|$)",
+        RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.ExplicitCapture,
+        TimeSpan.FromMilliseconds(100));
+}
