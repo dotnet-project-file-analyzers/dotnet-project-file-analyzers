39452a952016d5fc6eb39876f40e681c3545bc82
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-08-21 21:45:38 +0200

    Add rule

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Place_xml_nodes_on_seperate_lines.cs
@@ -0,0 +1,24 @@
+﻿namespace Rules.MS_Build.Place_xml_nodes_on_seperate_lines;
+
+public class Reports
+{
+    [Test]
+    public void on_same_line()
+       => new PlaceXmlNodesOnSeperateLines()
+       .ForProject("MultipleXmlNodesOnSingleLine.cs")
+       .HasIssues(
+           new Issue("Proj7001", "Move XML node '<TargetFramework>net8.0</TargetFramework>' to a new line.").WithSpan(2, 18, 2, 19),
+           new Issue("Proj7001", "Move XML node '<PackageReference Include=\"Microsoft.CodeAnalysis.Workspaces.Common\" Version=\"4.10.0\" />' to a new line.").WithSpan(4, 14, 4, 101),
+           new Issue("Proj7001", "Move XML node '<PackageReference Include=\"Qowaiv.Analyzers.CSharp\" Version=\"2.0.0\" />' to a new line.").WithSpan(6, 5, 6, 6),
+           new Issue("Proj7001", "Move XML node '<Compile Include=\"../common/Code.cs\" />' to a new line.").WithSpan(9, 5, 9, 6));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void Projects_without_issues(string project)
+         => new PlaceXmlNodesOnSeperateLines()
+        .ForProject(project)
+        .HasNoIssues();
+}
--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/PlaceXmlNodesOnSeperateLines.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/PlaceXmlNodesOnSeperateLines.cs
@@ -1,16 +1,101 @@
-﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+﻿using System.IO;
+using System.Xml;
+
+namespace DotNetProjectFile.Analyzers.MsBuild;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
 public sealed class PlaceXmlNodesOnSeperateLines() : MsBuildProjectFileAnalyzer(Rule.PlaceXmlNodesOnSeperateLines)
 {
     protected override void Register(ProjectFileAnalysisContext context)
     {
-        if (context.Project
-            .ImportsAndSelf()
-            .SelectMany(p => p.PropertyGroups)
-            .SelectMany(g => g.IsPackable).None())
+        var nodes = GetFlatChildren(context.Project).ToArray();
+        var occupied = new Dictionary<int, Node>();
+
+        foreach (var node in nodes)
         {
-            context.ReportDiagnostic(Descriptor, context.Project);
+            (var startLine, var endLine) = GetLineNumbers(node);
+
+            if (occupied.TryGetValue(startLine, out var prev) && prev != node)
+            {
+                context.ReportDiagnostic(Descriptor, node, node.ToString());
+                continue;
+            }
+            else
+            {
+                occupied[startLine] = node;
+            }
+
+            if (occupied.TryGetValue(endLine, out prev) && prev != node)
+            {
+                context.ReportDiagnostic(Descriptor, node, node.ToString());
+            }
+            else
+            {
+                occupied[endLine] = node;
+            }
         }
     }
+
+    private static IEnumerable<Node> GetFlatChildren(Node node)
+    {
+        foreach (var child in node.Children)
+        {
+            yield return child;
+            foreach (var grandchild in GetFlatChildren(child))
+            {
+                yield return grandchild;
+            }
+        }
+    }
+
+#pragma warning disable S3776 // Max complexity of 15. Justification: Logic state machine relatively well contained.
+    private static (int StartLine, int EndLine) GetLineNumbers(Node node)
+    {
+        using XmlReader reader = node.Element.CreateReader();
+
+        if (reader is not IXmlLineInfo lineInfo)
+        {
+            return (node.LineInfo.LineNumber, node.LineInfo.LineNumber);
+        }
+
+        var startLine = -1;
+        var endLine = -1;
+        var depth = 0;
+
+        while (reader.Read())
+        {
+            if (reader.LocalName != node.Element.Name.LocalName)
+            {
+                continue;
+            }
+
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                if (depth == 0)
+                {
+                    startLine = lineInfo.LineNumber;
+
+                    if (reader.IsEmptyElement)
+                    {
+                        return (startLine, startLine);
+                    }
+                }
+
+                depth++;
+            }
+            else if (reader.NodeType == XmlNodeType.EndElement)
+            {
+                depth--;
+
+                if (depth == 0)
+                {
+                    endLine = lineInfo.LineNumber;
+                    break;
+                }
+            }
+        }
+
+        return (startLine, endLine);
+    }
+#pragma warning restore S3776
 }
