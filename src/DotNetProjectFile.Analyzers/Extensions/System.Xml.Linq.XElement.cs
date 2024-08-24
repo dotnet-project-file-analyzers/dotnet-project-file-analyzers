786188bc29c59de78a262f8069237ef4cb55b11e
Corniel Nobel <corniel@gmail.com>
2024-08-24 21:40:37 +0200

    Implement IsSelfclosing.

---
+++ src/DotNetProjectFile.Analyzers/Extensions/System.Xml.Linq.XElement.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Xml.Linq;
+
+internal static class XElementExtensions
+{
+    public static bool IsSelfClosing(this XElement element)
+    {
+        if (element.Value is not { Length: > 0 })
+        {
+            return element.ToString()[^2] == '/';
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
