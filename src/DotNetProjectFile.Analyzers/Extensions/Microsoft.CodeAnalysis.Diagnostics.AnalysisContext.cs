1d1e17bbe628519012d0f5485d9b601d13232832
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-08-27 13:59:53 +0200

    Use AdditionalFileAction for AdditionalFiles (#138)

--- src/DotNetProjectFile.Analyzers/Extensions/Microsoft.CodeAnalysis.Diagnostics.AnalysisContext.cs
+++ src/DotNetProjectFile.Analyzers/Extensions/Microsoft.CodeAnalysis.Diagnostics.AnalysisContext.cs
@@ -5,7 +5,8 @@ internal static class AnalysisContextExtensions
 {
     /// <summary>Registers an action on <see cref="ProjectFileAnalysisContext"/>.</summary>
     public static void RegisterProjectFileAction(this AnalysisContext context, Action<ProjectFileAnalysisContext> action)
-        => context.RegisterCompilationAction(c =>
+    {
+        context.RegisterAdditionalFileAction(c =>
         {
             if (Projects.Init(c).EntryPoint(c) is { } project
                 && string.IsNullOrEmpty(project.Element.Name.NamespaceName))
@@ -17,9 +18,23 @@ internal static class AnalysisContextExtensions
             }
         });
 
+        // Fallback for detecting files that not have been added as additional files.
+        context.RegisterCompilationAction(c =>
+        {
+            if (Projects.Init(c).EntryPoint(c) is { IsAdditional: false } project
+                && string.IsNullOrEmpty(project.Element.Name.NamespaceName))
+            {
+                foreach (var p in project.ImportsAndSelf())
+                {
+                    action.Invoke(new(p, c.Compilation, c.Options, c.CancellationToken, c.ReportDiagnostic));
+                }
+            }
+        });
+    }
+
     /// <summary>Registers an action on <see cref="ProjectFileAnalysisContext"/>.</summary>
     public static void RegisterResourceFileAction(this AnalysisContext context, Action<ResourceFileAnalysisContext> action)
-        => context.RegisterCompilationAction(c =>
+        => context.RegisterAdditionalFileAction(c =>
         {
             foreach (var resource in DotNetProjectFile.Resx.Resources.Resolve(c.Compilation, c.Options.AdditionalFiles).Where(r => r.IsXml))
             {
