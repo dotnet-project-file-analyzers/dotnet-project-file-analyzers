6fa1e8512c240bf3a448bfdb5e517759444d5fd9
Corniel Nobel <corniel@gmail.com>
2024-08-27 13:11:20 +0200

    Extra test case.

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Indent_XML.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Indent_XML.cs
@@ -7,10 +7,11 @@ public class Reports
         => new IndentXml()
         .ForProject("MaliciousIndenting.cs")
         .HasIssues(
-            new Issue("Proj1700", "The element <PropertyGroup> has not been properly indented." /*...*/).WithSpan(4, 04, 4, 18),
-            new Issue("Proj1700", "The element <TargetFramework> has not been properly indented." /*.*/).WithSpan(5, 02, 5, 18),
-            new Issue("Proj1700", "The element </PropertyGroup> has not been properly indented." /*..*/).WithSpan(6, 05, 6, 21),
-            new Issue("Proj1700", "The element <Compile> has not been properly indented." /*.........*/).WithSpan(9, 04, 9, 43));
+            new Issue("Proj1700", "The element <PropertyGroup> has not been properly indented." /*...*/).WithSpan(04, 04, 04, 18),
+            new Issue("Proj1700", "The element <TargetFramework> has not been properly indented." /*.*/).WithSpan(05, 02, 05, 18),
+            new Issue("Proj1700", "The element </PropertyGroup> has not been properly indented." /*..*/).WithSpan(06, 05, 06, 21),
+            new Issue("Proj1700", "The element <Compile> has not been properly indented." /*.........*/).WithSpan(09, 04, 09, 43),
+            new Issue("Proj1700", "The element <Folder> has not been properly indented." /*..........*/).WithSpan(12, 13, 12, 21));
 }
 
 public class Guards
