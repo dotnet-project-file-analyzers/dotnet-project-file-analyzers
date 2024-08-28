099c07668e05c0c109a7ce32b010c541cd3a7e28
Corniel Nobel <corniel@gmail.com>
2024-08-28 11:16:13 +0200

    Package references should have stable versions, so no nightly builds and other pre-releases.

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/PackageReferencesShouldBeStable.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class PackageReferencesShouldBeStable() : MsBuildProjectFileAnalyzer(Rule.PackageReferencesShouldBeStable)
+{
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        foreach (var package in context.Project.ItemGroups
+            .SelectMany(i => i.PackageReferences)
+            .Where(IsUnstable))
+        {
+            context.ReportDiagnostic(Descriptor, package, package.IncludeOrUpdate, package.Version);
+        }
+    }
+
+    private static bool IsUnstable(PackageReference package)
+        => package.Version is { Length: > 0 } version
+        && version.Contains('-')
+        && !"ALL".Equals(package.PrivateAssets, StringComparison.OrdinalIgnoreCase);
+}
