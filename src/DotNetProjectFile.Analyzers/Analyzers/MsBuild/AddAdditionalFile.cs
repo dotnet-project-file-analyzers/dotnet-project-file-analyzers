ca6d9cc0162c1be18286bcdf6de9b6065f70dab5
Corniel Nobel <corniel@gmail.com>
2024-08-30 09:14:22 +0200

    Use ApplicableTo,

--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/AddAdditionalFile.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/AddAdditionalFile.cs
@@ -1,15 +1,16 @@
-﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+﻿
+namespace DotNetProjectFile.Analyzers.MsBuild;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
 public sealed class AddAdditionalFile() : MsBuildProjectFileAnalyzer(Rule.AddAdditionalFile)
 {
+    protected override IReadOnlyCollection<ProjectFileType> ApplicableTo => ProjectFileTypes.ProjectFile_Props;
+
     protected override void Register(ProjectFileAnalysisContext context)
     {
-        if (!context.Project.IsAdditional && FileTypes.Contains(context.Project.FileType))
+        if (!context.Project.IsAdditional)
         {
             context.ReportDiagnostic(Descriptor, context.Project, context.Project.Path.Name);
         }
     }
-
-    private static readonly ProjectFileType[] FileTypes = [ProjectFileType.ProjectFile, ProjectFileType.Props];
 }
