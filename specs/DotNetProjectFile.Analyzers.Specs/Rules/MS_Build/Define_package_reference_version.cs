6158be31efc9d585e9af8d0b2158525c8a02ed21
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-08-31 20:55:47 +0200

    Define PackageReference version (#152)
    * Define PackageReference version
    
    * no message
    
    * Change to 0805
    
    * Other comments
    
    * no message

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Define_package_reference_version.cs
@@ -0,0 +1,21 @@
+﻿namespace Rules.MS_Build.Define_package_reference_version;
+
+public class Reports
+{
+    [Test]
+    public void on_no_output_type()
+       => new DefinePackageReferenceVersion()
+       .ForProject("PackageReferenceWithoutVersion.cs")
+       .HasIssue(
+           new Issue("Proj0805", "Define version for 'Warpstone' PackageReference."));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void Projects_without_issues(string project)
+         => new DefinePackageReferenceVersion()
+        .ForProject(project)
+        .HasNoIssues();
+}
