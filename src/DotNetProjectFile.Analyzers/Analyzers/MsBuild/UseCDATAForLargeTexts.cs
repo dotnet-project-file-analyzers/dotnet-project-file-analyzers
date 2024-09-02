09800fc42060a4be5cc53ca1f3ddb1298989ba45
Corniel Nobel <corniel@gmail.com>
2024-09-02 18:47:31 +0200

    Proj1701: Use <![CDATA[ for large texts (#157)

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UseCDATAForLargeTexts.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetProjectFile.Analyzers.MsBuild;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class UseCDATAForLargeTexts() : MsBuildProjectFileAnalyzer(Rule.UseCDATAForLargeTexts)
+{
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        Walk(context.Project);
+
+        void Walk(Node node)
+        {
+            if (node is PackageReleaseNotes && !node.Element.ContainsCDATA())
+            {
+                context.ReportDiagnostic(Descriptor, node.Positions.InnerSpan);
+            }
+
+            foreach (var child in node.Children)
+            {
+                Walk(child);
+            }
+        }
+    }
+}
