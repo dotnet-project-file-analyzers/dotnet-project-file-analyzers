c109a50a735dbe87492216d8044328f66ff4623e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-09-03 18:23:25 +0200

    Add 47 more private assets

---
+++ src/DotNetProjectFile.Analyzers/NuGet/SourceGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetProjectFile.NuGet;
+
+public sealed record SourceGenerator : Package
+{
+    public SourceGenerator(string name, string? language = null)
+        : base(name: name, language: language, isPrivateAsset: true)
+    {
+    }
+}
