681ffa9731f3b5176c385e1bbcd9500b55b3814c
Corniel Nobel <corniel@gmail.com>
2024-09-11 16:23:11 +0200

    Enable <GenerateDocumentationFile> for DotNetProjectFile.Analyzers.csproj (#192)

--- src/DotNetProjectFile.Analyzers/IO/IOFile.cs
+++ src/DotNetProjectFile.Analyzers/IO/IOFile.cs
@@ -1,5 +1,4 @@
-﻿#nullable enable
-#pragma warning disable CA2231 // Overload operator equals on overriding value type Equals
+﻿#pragma warning disable CA2231 // Overload operator equals on overriding value type Equals
 #pragma warning disable S1210 // "Equals" and the comparison operators should be overridden when implementing "IComparable"
 
 using System.ComponentModel;
