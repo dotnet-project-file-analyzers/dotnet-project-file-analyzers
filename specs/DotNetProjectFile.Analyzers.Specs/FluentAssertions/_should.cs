db9070e13217e66ab165b97b757eaca3a51a08c1
Corniel Nobel <corniel@gmail.com>
2024-09-14 16:52:38 +0200

    Add tests.

---
+++ specs/DotNetProjectFile.Analyzers.Specs/FluentAssertions/_should.cs
@@ -0,0 +1,10 @@
+﻿using DotNetProjectFile.Parsing;
+
+namespace FluentAssertions;
+
+internal static class DotNetProjectFIleFLuentAssertionsExtensions
+{
+    [Pure]
+    public static ParserAssertions Should(this Parser parser) => new(parser);
+
+}
