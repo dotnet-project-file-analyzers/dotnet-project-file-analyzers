335fc9c98859c05ddec47805dd37a06fe3fce199
Corniel Nobel <corniel@gmail.com>
2024-09-15 18:40:37 +0200

    Add benchmark results.

---
+++ specs/Benchmarks/INI.cs
@@ -0,0 +1,27 @@
+﻿using DotNetProjectFile.Ini;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Benchmarks;
+
+public class INI
+{
+    private static readonly string root = string.Join("/", Enumerable.Repeat("..", 7)) + "/Files/";
+
+    private readonly List<SourceText> Texts = [];
+    
+    public INI()
+    {
+        string[] files = [ "ini-0027-lines.ini", "ini-0036-lines.ini", "ini-1220-lines.ini" ];
+        foreach(var file in files)
+        {
+            using var stream = new FileStream(root + file, FileMode.Open, FileAccess.Read);
+            Texts.Add(SourceText.From(stream));
+        }
+    }
+
+    [Params(0, 1, 2)]
+    public int Index { get; set; }
+
+    [Benchmark]
+    public IniFileSyntax Parse() => IniFileSyntax.Parse(Texts[Index]);
+}
