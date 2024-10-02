0ef3f868abedd9c7e2669f5cd9db36cfadd057ef
Corniel Nobel <corniel@gmail.com>
2024-10-02 10:36:34 +0200

    Where using BOM.

--- src/DotNetProjectFile.Analyzers/IO/ProjectTextFile.cs
+++ src/DotNetProjectFile.Analyzers/IO/ProjectTextFile.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Text;
 
 namespace DotNetProjectFile.IO;
 
+/// <summary>Represents a (generic) project text file.</summary>
 public sealed class ProjectTextFile(IOFile path) : ProjectFile
 {
+    /// <inheritdoc />
     public IOFile Path { get; } = path;
 
+    /// <inheritdoc />
     public SourceText Text => SourceText.From(Path.OpenRead());
 
+    /// <inheritdoc />
     public WarningPragmas WarningPragmas { get; } = WarningPragmas.None;
 }
