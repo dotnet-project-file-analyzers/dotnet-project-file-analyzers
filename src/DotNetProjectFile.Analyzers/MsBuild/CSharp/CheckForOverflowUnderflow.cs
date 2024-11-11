5ce79be7e47a907938b4845ede0fd7f815b7cd0c
Corniel Nobel <corniel@gmail.com>
2024-11-11 12:50:39 +0100

    Implement C# only analyzer.

---
+++ src/DotNetProjectFile.Analyzers/MsBuild/CSharp/CheckForOverflowUnderflow.cs
@@ -0,0 +1,9 @@
+namespace DotNetProjectFile.MsBuild.CSharp;
+
+/// <summary>
+/// The CheckForOverflowUnderflow option controls the default overflow-checking
+/// context that defines the program behavior if integer arithmetic overflows.
+/// </summary>
+/// <remarks>C# only.</remarks>
+public sealed class CheckForOverflowUnderflow(XElement element, Node parent, MsBuildProject project)
+    : Node<bool?>(element, parent, project) { }
---
+++ src/DotNetProjectFile.Analyzers/MsBuild/CSharp/Nullable.cs
@@ -0,0 +1,8 @@
+namespace DotNetProjectFile.MsBuild.CSharp;
+
+/// <summary>
+/// Enable nullable context, or nullable warnings.
+/// </summary>
+/// <remarks>C# only.</remarks>
+public sealed class Nullable(XElement element, Node parent, MsBuildProject project)
+    : Node(element, parent, project) { }
