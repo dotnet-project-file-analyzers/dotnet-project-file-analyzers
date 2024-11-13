24b7a054b99c959e06c32fa5e8b44c5b648060fb
Corniel Nobel <corniel@gmail.com>
2024-11-13 19:32:08 +0100

    Fix test.

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/SDK/Does_not_report.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/SDK/Does_not_report.cs
@@ -7,10 +7,13 @@ public class On
         .ForSDkProject("DotnetProjectFilesSdk")
         .HasNoIssues();
 
+    /// <remarks>Imported files can be ignored.</remarks>>
     [Test]
     public void Project_not_available_as_additional() => new AddAdditionalFile()
         .ForSDkProject("DotnetProjectFilesSdk")
-        .HasNoIssues();
+        .HasIssues(
+            new Issue("Proj0006", "Add 'DotNetProjectFile.Analyzers.Sdk.props' to the additional files."),
+            new Issue("Proj0006", "Add 'DotNetProjectFile.Analyzers.Sdk.targets' to the additional files."));
 
     [Test]
     public void Unresolved_Project() => new GuardUnsupported()
