a19e6dd587119d02fc6898739c33321a28c9f2ed
Corniel Nobel <corniel@gmail.com>
2024-11-18 11:18:54 +0100

    Tweak grammar

--- src/DotNetProjectFile.Analyzers/Ini/_grammar.cs
+++ src/DotNetProjectFile.Analyzers/Ini/_grammar.cs
@@ -11,38 +11,39 @@ internal sealed class IniGrammar : Grammar
 
     public static readonly Grammar ws_only = line(@"^\s*$", WhitespaceToken);
 
-    public static readonly Grammar header =
-        (ws
-        & ch('[', HeaderStartToken)
-        & line(@"[^]]+", HeaderTextToken)
-        & ch(']', HeaderEndToken)
-        & ws
-        & eol)
-        + HeaderSyntax.New;
-
-    public static readonly Grammar unparsable = ~header & line(".*", UnparsableToken);
-
     public static readonly Grammar comment =
         ws
         & (ch('#', CommentDelimiterToken) | ch(';', CommentDelimiterToken))
         & line(".*", CommentToken);
 
-    public static readonly Grammar key = line(@"[^\s:=]+", KeyToken) + KeySyntax.New;
-
-    public static readonly Grammar assign = ch('=', EqualsToken) | ch(':', ColonToken);
-
-    public static readonly Grammar value = line(@"[^\s#;]+", ValueToken) + ValueSyntax.New;
+    public static readonly Grammar header =
+        (ws
+        & Header.start
+        & Header.text
+        & Header.end
+        & ws
+        & comment.Option
+        & eol)
+        + HeaderSyntax.New;
 
     public static readonly Grammar kvp =
         (ws
-        & key
+        & INI.key
         & ws
-        & assign
+        & INI.assign
         & ws
-        & value
+        & INI.value
         & ws
         & comment.Option)
         + KeyValuePairSyntax.New;
+
+    /// <remarks>
+    /// Unparsable is separated from the happy-fow for performance reasons. 
+    /// </remarks>
+    public static readonly Grammar unparsable =
+        Invalid.header
+        | Invalid.kvp
+        | line(".*", UnparsableToken);
 
     public static readonly Grammar single_line =
         (kvp
@@ -54,20 +55,62 @@ internal sealed class IniGrammar : Grammar
     public static readonly Grammar section = (header & single_line.Star) + SectionSyntax.New;
 
     public static readonly Grammar file = (single_line.Star & section.Star) + IniFileSyntax.New;
+
+    /// <summary>Header tokens.</summary>
+    private sealed class Header : Grammar
+    {
+        public static Grammar start = ch('[', HeaderStartToken);
+        public static Grammar text = line(@"[^[\]]+", HeaderTextToken);
+        public static Grammar end = ch(']', HeaderEndToken);
+    }
+
+    /// <summary>INI tokens.</summary>
+    private sealed class INI : Grammar
+    {
+        public static readonly Grammar key = line(@"[^\s:=]+", KeyToken) + KeySyntax.New;
+
+        public static readonly Grammar assign = ch('=', EqualsToken) | ch(':', ColonToken);
+
+        public static readonly Grammar value = line(@"[^\s#;]+", ValueToken) + ValueSyntax.New;
+    }
+
+    private sealed class Invalid : Grammar
+    {
+        public static readonly Grammar header =
+           (ws
+           & Header.start.Plus
+           & Header.text.Option
+           & Header.end.Star
+           & ws
+           & comment.Option
+           & eol)
+           + HeaderSyntax.New;
+
+        public static readonly Grammar kvp =
+           (ws
+           & INI.key.Star
+           & ws
+           & INI.assign.Star
+           & ws
+           & INI.value.Option
+           & ws
+           & comment.Option)
+           + KeyValuePairSyntax.New;
+    }
 }
 
 public static class TokenKind
 {
-    public static readonly string ValueToken = nameof(ValueToken);
-    public static readonly string KeyToken = nameof(KeyToken);
-    public static readonly string WhitespaceToken = nameof(WhitespaceToken);
-    public static readonly string CommentDelimiterToken = nameof(CommentDelimiterToken);
-    public static readonly string CommentToken = nameof(CommentToken);
-    public static readonly string EoLToken = nameof(EoLToken);
-    public static readonly string HeaderTextToken = nameof(HeaderTextToken);
-    public static readonly string HeaderStartToken = nameof(HeaderStartToken);
-    public static readonly string HeaderEndToken = nameof(HeaderEndToken);
-    public static readonly string EqualsToken = nameof(EqualsToken);
-    public static readonly string ColonToken = nameof(ColonToken);
-    public static readonly string UnparsableToken = nameof(UnparsableToken);
+    public const string ValueToken = nameof(ValueToken);
+    public const string KeyToken = nameof(KeyToken);
+    public const string WhitespaceToken = nameof(WhitespaceToken);
+    public const string CommentDelimiterToken = nameof(CommentDelimiterToken);
+    public const string CommentToken = nameof(CommentToken);
+    public const string EoLToken = nameof(EoLToken);
+    public const string HeaderTextToken = nameof(HeaderTextToken);
+    public const string HeaderStartToken = nameof(HeaderStartToken);
+    public const string HeaderEndToken = nameof(HeaderEndToken);
+    public const string EqualsToken = nameof(EqualsToken);
+    public const string ColonToken = nameof(ColonToken);
+    public const string UnparsableToken = nameof(UnparsableToken);
 }
