747a85b654bd33173d4413e6c7d5c98bbeeca63c
Corniel Nobel <corniel@gmail.com>
2024-11-19 11:05:13 +0100

    Add Proj4000.

--- src/DotNetProjectFile.Analyzers/Rule.Ini.cs
+++ src/DotNetProjectFile.Analyzers/Rule.Ini.cs
@@ -4,19 +4,27 @@ public static partial class Rule
 {
     public static class Ini
     {
+        public static DiagnosticDescriptor Invalid => New(
+           id: 4000,
+           title: "Invalid INI file",
+           message: "File could not be parsed",
+           description: "A INI header should have the format [<Header>].",
+           tags: ["INI", "syntax error"],
+           category: Category.SyntaxError);
+
         public static DiagnosticDescriptor InvalidHeader => New(
            id: 4001,
            title: "Invalid INI Header",
            message: "{0}",
-           description: "A INI header should be of the format [<Header>].",
+           description: "A INI header should have the format [<Header>].",
            tags: ["INI", "syntax error"],
            category: Category.SyntaxError);
 
         public static DiagnosticDescriptor InvalidKeyValuePair => New(
            id: 4002,
-           title: "Invalid INI key value pair",
+           title: "Invalid INI key-value pair",
            message: "{0}",
-           description: "A INI key value pair should be of the format <Key> ( : | = ) <Value>.",
+           description: "A INI key-value pair should have the format <Key> ( : | = ) <Value>.",
            tags: ["INI", "syntax error"],
            category: Category.SyntaxError);
     }
