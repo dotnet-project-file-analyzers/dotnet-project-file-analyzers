593b80d3a3518777ef90a81f80cc41baec416689
Corniel Nobel <corniel@gmail.com>
2024-11-21 17:22:40 +0100

    Proj4010: Sections should contain a key-value pair

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/INI/Empty_section.cs
@@ -0,0 +1,21 @@
+using DotNetProjectFile.Analyzers.Ini;
+
+namespace Rules.INI.Empty_section;
+
+public class Reports
+{
+    [Test]
+    public void empty_sections() => new EmptySection()
+        .ForProject("IniFileWithEmptySection.cs")
+        .HasIssue(Issue.WRN("Proj4010", "Section [empty.*] is empty.").WithSpan(09, 00, 10, 00));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void Projects_without_issues(string project) => new EmptySection()
+        .ForProject(project)
+        .HasNoIssues();
+}
+
