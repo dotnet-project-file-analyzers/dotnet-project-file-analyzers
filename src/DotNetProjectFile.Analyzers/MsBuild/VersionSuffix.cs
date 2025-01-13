8a36dbc013c9fd94c5852ba56c48a22f76905e37
Wesley Baartman <cptwesley@jojoweba.nl>
2025-01-13 12:21:02 +0200

    Fix bom

--- src/DotNetProjectFile.Analyzers/MsBuild/VersionSuffix.cs
+++ src/DotNetProjectFile.Analyzers/MsBuild/VersionSuffix.cs
@@ -1,4 +1,4 @@
-﻿namespace DotNetProjectFile.MsBuild;
+namespace DotNetProjectFile.MsBuild;
 
 public sealed class VersionSuffix(XElement element, Node parent, MsBuildProject project)
     : Node<string>(element, parent, project) { }
