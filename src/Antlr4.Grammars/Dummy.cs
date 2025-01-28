02bfff66493fa63d632879b649a204330c10ce27
Wesley Baartman <cptwesley@jojoweba.nl>
2025-01-28 11:02:31 +0100

    no message

---
+++ src/Antlr4.Grammars/Dummy.cs
@@ -0,0 +1,14 @@
+using DotNetProjectFile.Ini;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antlr4.Grammars
+{
+    internal class Dummy
+    {
+        IniParser foo = null!;
+    }
+}
