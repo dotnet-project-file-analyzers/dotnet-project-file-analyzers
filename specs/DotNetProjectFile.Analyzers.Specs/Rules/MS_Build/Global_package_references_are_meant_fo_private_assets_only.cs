8c884c35469e6b28e98463c0a558783518c8322b
Corniel Nobel <corniel@gmail.com>
2025-02-13 09:34:31 +0100

    Add test for Proj0809

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Global_package_references_are_meant_fo_private_assets_only.cs
@@ -0,0 +1,31 @@
+namespace Rules.MS_Build.Global_package_references_are_meant_fo_private_assets_only;
+
+public class Reports
+{
+    [Test]
+    public void on_global_package_references_in_csproj() => new GlobalPackageReferencesAreMeantForPrivateAssetsOnly()
+        .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+    <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    </PropertyGroup>
+
+  <ItemGroup>
+    <GlobalPackageReference Include=""DotNetProjectFile.Analyzers"" Version=""1.5.8"" />
+  </ItemGroup>
+
+</Project>")
+        .HasIssue(
+            Issue.WRN("Proj0808", "The <GlobalPackageReference> should be defined in the Directory.Packages.props.").WithSpan(07, 04, 07, 84));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    [TestCase("UseCPM.cs")]
+    public void Projects_without_issues(string project) => new GlobalPackageReferencesAreMeantForPrivateAssetsOnly()
+        .ForProject(project)
+        .HasNoIssues();
+}
