ca4b02becdb528e38b11ccb7d684980b8106df97
Corniel Nobel <corniel@gmail.com>
2025-02-17 14:04:41 +0100

    Skip rules that can not be executed when an import could not be resolved

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Unresolvable_Import.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Unresolvable_Import.cs
@@ -1,17 +1,20 @@
-using DotNetProjectFile.MsBuild;
-
 namespace Rules.MS_Build.Unresolvable_Import;
 
 public class Reports
 {
     [Test]
-    public void empty_includes() => new UnresolvableImport()
+    public void on_rule_that_is_not_affected() => new TrackToDoTags()
     .ForProject("UnresolvableImport.cs")
-        .HasIssue(Issue.WRN("Proj0034", "The <Import> '$(MSBuildThisFileDirectory)common.props' could not be resolved by the analyzer.").WithSpan(02, 02, 02, 62));
+        .HasIssue(Issue.WRN("Proj3001", "Complete the task associated to this \"TODO\" comment."));
 }
 
 public class Guards
 {
+    [Test]
+    public void rule_that_is_affected() => new ConfigureCentralPackageVersionManagement()
+    .ForProject("UnresolvableImport.cs")
+        .HasNoIssues();
+
     [TestCase("CompliantCSharp.cs")]
     [TestCase("CompliantCSharpPackage.cs")]
     public void Projects_with_analyzers(string project) => new UnresolvableImport()
--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UnresolvableImport.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UnresolvableImport.cs
@@ -1,11 +1,13 @@
-using DotNetProjectFile.Analyzers;
-
-namespace DotNetProjectFile.MsBuild;
+namespace DotNetProjectFile.Analyzers.MsBuild;
 
 /// <summary>Implements <see cref="Rule.UnresolvableImport"/>.</summary>
 [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
 public sealed class UnresolvableImport() : MsBuildProjectFileAnalyzer(Rule.UnresolvableImport)
 {
+    /// <inheritdoc />
+    public override bool DisableOnFailingImport => false;
+
+    /// <inheritdoc />
     protected override void Register(ProjectFileAnalysisContext<MsBuildProject> context)
     {
         foreach (var import in context.File.Imports.Where(i => i.Value is null))
