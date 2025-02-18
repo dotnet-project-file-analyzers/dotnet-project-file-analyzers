5c447eb521be70d1acccb951d7a39df45a99e985
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-02-18 21:07:13 +0100

    no message

---
+++ src/DotNetProjectFile.Analyzers/Licensing/SpdxLicenseInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetProjectFile.Licensing;
+
+public sealed record SpdxLicenseInfo
+{
+    public required string Name { get; init; }
+
+    public required string Id { get; init; }
+
+    public required ImmutableArray<string> SeeAlso { get; init; }
+
+    public required string LicenseText { get; init; }
+
+    public required bool Osi { get; init; }
+
+    public required bool Fsf { get; init; }
+}
