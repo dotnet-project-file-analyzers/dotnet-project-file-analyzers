1c941f7c805be8d582a9f3e261e97e30a352d783
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-02-18 22:28:28 +0100

    Add lookup table generation for SpdxLicenseInfos

--- src/DotNetProjectFile.Analyzers/Licensing/Spdx.cs
+++ src/DotNetProjectFile.Analyzers/Licensing/Spdx.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Frozen;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace DotNetProjectFile.Licensing;
 
 public static partial class Spdx
 {
+    public static readonly ImmutableArray<SpdxLicenseInfo> Licenses
+        = typeof(Spdx)
+        .GetRuntimeFields()
+        .Where(f => f.FieldType == typeof(SpdxLicenseInfo))
+        .Select(f => f.GetValue(null))
+        .OfType<SpdxLicenseInfo>()
+        .ToImmutableArray();
+
+    private static readonly FrozenDictionary<string, SpdxLicenseInfo> Lookup
+        = Licenses.ToFrozenDictionary(x => x.Id, x => x);
+
+    public static SpdxLicenseInfo? TryGetLicense(string? id)
+    {
+        if (id is not { Length: > 0 })
+        {
+            return null;
+        }
+
+        return Lookup.TryGetValue(id, out var license)
+            ? license
+            : null;
+    }
 }
