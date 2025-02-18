d24a3d824634622e2768f95ce63c05d9fa03e2e6
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-02-18 22:22:45 +0100

    Add generator

--- src/DotNetProjectFile.Analyzers/Licensing/SpdxLicenseInfo.cs
+++ src/DotNetProjectFile.Analyzers/Licensing/SpdxLicenseInfo.cs
@@ -10,11 +10,11 @@ public sealed record SpdxLicenseInfo
 
     public required string Id { get; init; }
 
-    public required ImmutableArray<string> SeeAlso { get; init; }
-
-    public required string LicenseText { get; init; }
-
     public required bool Osi { get; init; }
 
     public required bool Fsf { get; init; }
+
+    public required ImmutableArray<string> SeeAlso { get; init; }
+
+    public required string? LicenseText { get; init; }
 }
