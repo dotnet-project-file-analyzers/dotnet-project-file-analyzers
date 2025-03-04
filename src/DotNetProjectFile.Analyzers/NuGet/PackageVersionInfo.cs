5c8f323d05e0646bffd4de4659bc857344a116c8
Corniel Nobel <corniel@gmail.com>
2025-03-04 13:28:42 +0100

    Make PackageVersionInfo case-insensitive

--- src/DotNetProjectFile.Analyzers/NuGet/PackageVersionInfo.cs
+++ src/DotNetProjectFile.Analyzers/NuGet/PackageVersionInfo.cs
@@ -1,3 +1,45 @@
 namespace DotNetProjectFile.NuGet;
 
-public readonly record struct PackageVersionInfo(string Name, string? Version);
+/// <summary>Represents the package name and (optional) version.</summary>
+public readonly struct PackageVersionInfo : IEquatable<PackageVersionInfo>
+{
+    public PackageVersionInfo(string name, string? version)
+    {
+        Name = name;
+        Version = version;
+    }
+
+    /// <summary>The name of the package.</summary>
+    public readonly string Name;
+
+    /// <summary>The version of the package.</summary>
+    public readonly string? Version;
+
+    /// <inheritdoc />
+    [Pure]
+    public override int GetHashCode()
+    {
+        var hash = Name?.ToUpperInvariant().GetHashCode() ?? 0;
+        hash *= 17;
+        hash ^= Version?.ToUpperInvariant().GetHashCode() ?? 0;
+        return hash;
+    }
+
+    /// <inheritdoc />
+    [Pure]
+    public override bool Equals(object? obj)=> obj is PackageVersionInfo other && Equals(other);
+
+    /// <inheritdoc />
+    [Pure]
+    public bool Equals(PackageVersionInfo other)
+        => Name.IsMatch(other.Name)
+        && Version.IsMatch(other.Version);
+
+    /// <inheritdoc />
+    [Pure]
+    public override string ToString() => Version is null ? Name : $"{Name} ({Version})";
+    
+    public static bool operator ==(PackageVersionInfo left, PackageVersionInfo right) => left.Equals(right);
+
+    public static bool operator !=(PackageVersionInfo left, PackageVersionInfo right) => !(left == right);
+}
