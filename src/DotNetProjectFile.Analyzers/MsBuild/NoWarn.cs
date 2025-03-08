541679edb1fc0715b56d8586be2b8718613cce3c
Corniel Nobel <corniel@gmail.com>
2025-03-08 16:14:12 +0100

    Trim results

--- src/DotNetProjectFile.Analyzers/MsBuild/NoWarn.cs
+++ src/DotNetProjectFile.Analyzers/MsBuild/NoWarn.cs
@@ -3,7 +3,7 @@ namespace DotNetProjectFile.MsBuild;
 public sealed class NoWarn(XElement element, Node parent, MsBuildProject project)
     : Node<string>(element, parent, project) 
 {
-    public IReadOnlyList<string> RuleIds
-        => Value?.Split(SemicolonSeparated, StringSplitOptions.RemoveEmptyEntries)
+    public IEnumerable<string> RuleIds
+        => Value?.Split(SemicolonSeparated, StringSplitOptions.RemoveEmptyEntries).Select(id => id.Trim())
         ?? [];
 }
