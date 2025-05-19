107a0ca699b3c84b22fb2ab7eac32e18a1501577
Corniel Nobel <corniel@gmail.com>
2025-05-19 20:00:33 +0200

    Proj0022: Include <EditorConfig> and <GlobalAnalyzerConfigFiles> as build actions (#426)

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Build_action_include_should_exist.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Build_action_include_should_exist.cs
@@ -13,6 +13,15 @@ public class Reports
             Issue.WRN("Proj0022", "The Include '**/*.bin' of <EmbeddedResource> does not match any files").WithSpan(11, 04, 11, 43),
             Issue.WRN("Proj0022", "The Include '*.unknown' of <AdditionalFiles> does not match any files").WithSpan(12, 04, 12, 43)
         );
+
+    [Test]
+    public void empty_includes_config_files()
+        => new BuildActionIncludeShouldExist()
+        .ForProject("ConfigFilesExistence.cs")
+        .HasIssues(
+            Issue.WRN("Proj0022", "The Include 'missing.editor' of <EditorConfgFiles> does not exist" /*....*/).WithSpan(12, 04, 12, 49),
+            Issue.WRN("Proj0022", "The Include 'missing.config' of <GlobalAnalyzerConfigFiles> does not exist").WithSpan(13, 04, 13, 58)
+        );
 }
 
 public class Guards
