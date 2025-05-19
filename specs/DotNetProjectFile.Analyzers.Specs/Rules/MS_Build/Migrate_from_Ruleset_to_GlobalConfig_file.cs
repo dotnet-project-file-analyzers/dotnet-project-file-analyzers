cc7aa2612f1330c56d5fd289dfbcdeffcf936832
Corniel Nobel <corniel@gmail.com>
2025-05-19 20:21:54 +0200

    rename

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Migrate_from_Ruleset_to_GlobalConfig_file.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Migrate_from_Ruleset_to_GlobalConfig_file.cs
@@ -1,10 +1,10 @@
-namespace Rules.MS_Build.Migrate_from_Ruleset_to_EditorConfig_file;
+namespace Rules.MS_Build.Migrate_from_Ruleset_to_GlobalConfig_file;
 
 public class Reports
 {
     [Test]
     public void private_assets_not_being_private()
-         => new MigrateFromRulesetToEditorConfigFile()
+         => new MigrateFromRulesetToGlobalConfigFile()
          .ForProject("WithRuleset.cs")
          .HasIssue(Issue.WRN("Proj0025", @"Migrate ruleset 'MyPreferences.ruleset' to a .globalconfig file")
          .WithSpan(04, 04, 04, 68));
@@ -15,7 +15,7 @@ public class Guards
     [TestCase("CompliantCSharp.cs")]
     [TestCase("CompliantCSharpPackage.cs")]
     public void Projects_with_analyzers(string project)
-         => new MigrateFromRulesetToEditorConfigFile()
+         => new MigrateFromRulesetToGlobalConfigFile()
         .ForProject(project)
         .HasNoIssues();
 }
