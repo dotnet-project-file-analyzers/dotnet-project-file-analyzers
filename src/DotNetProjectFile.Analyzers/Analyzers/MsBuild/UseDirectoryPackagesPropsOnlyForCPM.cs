d83967ed78d4b65e3322fe87e33e7412912dbd48
Corniel Nobel <corniel@gmail.com>
2025-05-29 19:32:35 +0200

    Proj0807: <TrirdPartyLicense> is allowed in Directory.Packages.props (#447)

--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UseDirectoryPackagesPropsOnlyForCPM.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UseDirectoryPackagesPropsOnlyForCPM.cs
@@ -33,6 +33,7 @@ public sealed class UseDirectoryPackagesPropsOnlyForCPM()
         ItemGroup => true,
         PackageVersion => true,
         GlobalPackageReference => true,
+        ThirdPartyLicense => true,
 
         // Properties
         ManagePackageVersionsCentrally => true,
