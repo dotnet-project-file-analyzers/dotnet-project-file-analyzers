03335eb7131873516ec80674dbddf0ccd713f100
Corniel Nobel <corniel@gmail.com>
2025-06-03 10:06:12 +0200

    Update src/DotNetProjectFile.Analyzers/Analyzers/SolutionFileAnalyzer.cs
    Co-authored-by: Wesley Baartman <9980136+CptWesley@users.noreply.github.com>

--- src/DotNetProjectFile.Analyzers/Analyzers/SolutionFileAnalyzer.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/SolutionFileAnalyzer.cs
@@ -3,7 +3,7 @@ using DotNetProjectFile.Slnx;
 namespace DotNetProjectFile.Analyzers;
 
 /// <summary>
-/// Base for <see cref="DiagnosticAnalyzer"/>s to analyze RESX resource files.
+/// Base for <see cref="DiagnosticAnalyzer"/>s to analyze SLNX solution files.
 /// </summary>
 public abstract class SolutionFileAnalyzer(
     DiagnosticDescriptor primaryDiagnostic,
