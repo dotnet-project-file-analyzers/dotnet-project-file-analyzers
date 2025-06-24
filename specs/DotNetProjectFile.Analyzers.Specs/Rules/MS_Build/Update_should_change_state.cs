fa19a3d69d077b14132db34295364f2451d74940
Corniel Nobel <corniel@gmail.com>
2025-06-24 21:11:38 +0200

    Proj0046: Update statements should change state (#480)
    Co-authored-by: Wesley Baartman <9980136+CptWesley@users.noreply.github.com>

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Update_should_change_state.cs
@@ -0,0 +1,63 @@
+namespace Rules.MS_Build.Update_should_change_state;
+
+public class Reports
+{
+    [Test]
+    public void on_update_with_single_attribute() => new UpdateShouldChangeState()
+        .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Update=""SonarAnalyzer.CSharp"" />
+  </ItemGroup>
+
+</Project>")
+        .HasIssue(
+            Issue.WRN("Proj0046", "Update statement does not change anything").WithSpan(7, 04, 7, 54));
+}
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void compliant(string project) => new UpdateShouldChangeState()
+        .ForProject(project)
+        .HasNoIssues();
+
+    [Test]
+    public void updates_via_child_elements() => new UpdateShouldChangeState()
+        .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+
+ <ItemGroup>
+    <Content Update=""*.md"">
+     <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </Content>
+  </ItemGroup>
+
+</Project>")
+        .HasNoIssues();
+
+    [Test]
+    public void updates_via_attributes() => new UpdateShouldChangeState()
+        .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+
+ <ItemGroup>
+    <PackageReference Update=""SonarAnalyzer.CSharp"" Version=""10.12"" />
+  </ItemGroup>
+
+</Project>")
+        .HasNoIssues();
+}
---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/UpdateShouldChangeState.cs
@@ -0,0 +1,28 @@
+
+namespace DotNetProjectFile.Analyzers.MsBuild;
+
+/// <summary>Implements <see cref="Rule.UpdateShouldChangeState"/>.</summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class UpdateShouldChangeState() : MsBuildProjectFileAnalyzer(Rule.UpdateShouldChangeState)
+{
+    /// <inheritdoc />
+    public override bool DisableOnFailingImport => false;
+
+    /// <inheritdoc />
+    protected override void Register(ProjectFileAnalysisContext<MsBuildProject> context)
+    {
+        foreach (var action in context.File.Project.ItemGroups
+            .Children<Node>(WithoutChanges))
+        {
+            context.ReportDiagnostic(Descriptor, action);
+        }
+    }
+
+    /// <summary>
+    /// True when the <see cref="BuildAction"/> only contains an Update attribute.
+    /// </summary>
+    private static bool WithoutChanges(Node action)
+        => action.Attribute("Update") is { }
+        && action.Element.Attributes().Skip(1).None()
+        && action.Element.Elements().None();
+}
