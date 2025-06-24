71925ae9ee8e2dea319a4ca68622bf12cdfdef21
Corniel Nobel <corniel@gmail.com>
2025-06-24 21:22:29 +0200

    Add testcase

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Global_package_references_are_meant_fo_private_assets_only.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Global_package_references_are_meant_fo_private_assets_only.cs
@@ -3,8 +3,7 @@ namespace Rules.MS_Build.Global_package_references_are_meant_fo_private_assets_o
 public class Reports
 {
     [Test]
-    public void global_package_reference_with_runtime_dependency()
-        => new GlobalPackageReferencesAreMeantForPrivateAssetsOnly()
+    public void global_package_reference_with_runtime_dependency() => new GlobalPackageReferencesAreMeantForPrivateAssetsOnly()
         .ForProject("GlobalPackageReferenceRuntimeDependency.cs")
         .HasIssues(
             Issue.WRN("Proj0809", @"The global package reference 'Qowaiv' is not supposed to be a private asset")
