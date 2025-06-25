326702f8f066c7a7c450e536175e04e88c05cbb9
Corniel Nobel <corniel@gmail.com>
2025-06-25 07:57:19 +0200

    Add testcase (#481)

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Remove_IncludeAssets_when_redundant.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Remove_IncludeAssets_when_redundant.cs
@@ -8,6 +8,28 @@ public class Reports
        .HasIssues(
            Issue.WRN("Proj0026", "Remove <IncludeAssets> as it is redundant when all assets are private" /*.*/).WithSpan(20, 04, 23, 23),
            Issue.WRN("Proj0026", "Remove IncludeAssets as it is redundant when all assets are private" /*...*/).WithSpan(24, 04, 24, 187));
+
+
+    [Test]
+    public void previously_reported_NuGet_Protocol() => new ValidatePrivateAssets()
+       .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Update=""SonarAnalyzer.CSharp"">
+      <PrivateAssets>all</PrivateAssets>
+      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+    </PackageReference>
+  </ItemGroup>
+
+</Project>")
+       .HasIssues(
+           Issue.WRN("Proj0026", "Remove <IncludeAssets> as it is redundant when all assets are private").WithSpan(07, 04, 10, 23),
+           Issue.WRN("Proj0037", "ExcludeAssets should contain runtime when PrivateAssets=\"all\"" /*.*/).WithSpan(07, 04, 10, 23));
 }
 
 public class Guards
