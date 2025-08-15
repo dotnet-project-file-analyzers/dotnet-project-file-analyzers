46bc27495bde1b897bbd4e2de06d63bfe10fa86e
Corniel Nobel <corniel@gmail.com>
2025-08-15 22:02:32 +0200

    Add test

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Label_item_groups_that_remove_compliation_items.cs
@@ -0,0 +1,42 @@
+namespace Rules.MS_Build.Label_item_groups_that_remove_compliation_items;
+
+public class Reports
+{
+    [Test]
+    public void unlabled_item_groups_with_compile_remove() => new LabelItemGroupsThatRemoveCompilationItems()
+       .ForInlineCsproj(@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <Compile Remove=""Program.cs"" />
+  </ItemGroup>
+
+  <ItemGroup Label="""">
+    <Compile Remove=""Program.cs"" />
+  </ItemGroup>
+
+  <ItemGroup Label=""With description"">
+    <Compile Remove=""Program.cs"" />
+  </ItemGroup>
+
+  <ItemGroup>
+    <Compile Include=""Other.cs"" />
+  </ItemGroup>
+
+</Project>")
+       .HasIssues(
+            Issue.WRN("Proj0047", "Add a label to this group as it removes items from compilation").WithSpan(05, 02, 07, 14),
+            Issue.WRN("Proj0047", "Add a label to this group as it removes items from compilation").WithSpan(09, 02, 11, 14));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void Projects_without_issues(string project) => new LabelItemGroupsThatRemoveCompilationItems()
+        .ForProject(project)
+        .HasNoIssues();
+}
