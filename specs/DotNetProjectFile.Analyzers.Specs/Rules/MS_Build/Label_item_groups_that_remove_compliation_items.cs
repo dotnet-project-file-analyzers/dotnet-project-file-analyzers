8bcacf47236983453ed93950645c8b0dd8b9a7a6
Corniel Nobel <corniel@gmail.com>
2025-08-16 20:59:35 +0200

    Raw quotes

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Label_item_groups_that_remove_compliation_items.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Label_item_groups_that_remove_compliation_items.cs
@@ -4,29 +4,30 @@ public class Reports
 {
     [Test]
     public void unlabled_item_groups_with_compile_remove() => new LabelItemGroupsThatRemoveCompilationItems()
-       .ForInlineCsproj(@"
-<Project Sdk=""Microsoft.NET.Sdk"">
+       .ForInlineCsproj(""""
+<Project Sdk="Microsoft.NET.Sdk">
   <PropertyGroup>
     <TargetFramework>net9.0</TargetFramework>
   </PropertyGroup>
 
   <ItemGroup>
-    <Compile Remove=""Program.cs"" />
+    <Compile Remove="Program.cs" />
   </ItemGroup>
 
-  <ItemGroup Label="""">
-    <Compile Remove=""Program.cs"" />
+  <ItemGroup Label="">
+    <Compile Remove="Program.cs" />
   </ItemGroup>
 
-  <ItemGroup Label=""With description"">
-    <Compile Remove=""Program.cs"" />
+  <ItemGroup Label="With description">
+    <Compile Remove="Program.cs" />
   </ItemGroup>
 
   <ItemGroup>
-    <Compile Include=""Other.cs"" />
+    <Compile Include="Other.cs" />
   </ItemGroup>
 
-</Project>")
+</Project>
+"""")
        .HasIssues(
             Issue.WRN("Proj0047", "Add a label to this group as it removes items from compilation").WithSpan(05, 02, 07, 14),
             Issue.WRN("Proj0047", "Add a label to this group as it removes items from compilation").WithSpan(09, 02, 11, 14));
