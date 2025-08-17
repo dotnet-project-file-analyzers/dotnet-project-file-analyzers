247c169ac2867e4565a6c48315ffb38f998c748d
Corniel Nobel <corniel@gmail.com>
2025-08-17 10:18:21 +0200

    Update src/DotNetProjectFile.Analyzers/Rule.cs
    Co-authored-by: Wesley Baartman <9980136+CptWesley@users.noreply.github.com>

--- src/DotNetProjectFile.Analyzers/Rule.cs
+++ src/DotNetProjectFile.Analyzers/Rule.cs
@@ -684,7 +684,7 @@ public static partial class Rule
     public static DiagnosticDescriptor PackablesShouldBeLibraries => New(
         id: 0219,
         title: "Packable projects should be libraries",
-        message: "The <IsPackable> node is set to 'true' but the <OutputType> is not 'library'",
+        message: "The <IsPackable> node is set to 'true' but the <OutputType> is not 'Library'",
         description: "Packing projects other than libraries is most likely a misstake.",
         tags: ["NuGet", "package"],
         category: Category.Bug);
