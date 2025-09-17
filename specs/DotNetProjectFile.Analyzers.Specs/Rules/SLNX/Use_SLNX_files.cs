be7ccb98d03b361298a6004188cf66edff7363c3
Corniel Nobel <corniel@gmail.com>
2025-09-17 11:04:30 +0200

    Simplify

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/SLNX/Use_SLNX_files.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/SLNX/Use_SLNX_files.cs
@@ -5,7 +5,7 @@ namespace Rules.SLNX.Use_SLNX_files;
 public class Reports
 {
     [Test]
-    public void net90_and_up_via_TargetFramework() => new UseSlnxFiles()
+    public void missing_SLNX() => new UseSlnxFiles()
        .ForInlineSdkProject("""
 <Project Sdk="Microsoft.NET.Sdk">
 
@@ -15,37 +15,5 @@ public class Reports
 
 </Project>
 """)
-       .HasIssue(
-           Issue.WRN("Proj5000", "Use a SLNX solution file instead"));
-
-    [Test]
-    public void net90_and_up_via_TargetFrameworks() => new UseSlnxFiles()
-     .ForInlineSdkProject("""
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <TargetFrameworks>net6.0;net9.0</TargetFrameworks>
-  </PropertyGroup>
-
-</Project>
-""")
-     .HasIssue(
-         Issue.WRN("Proj5000", "Use a SLNX solution file instead"));
-}
-
-public class Guards
-{
-
-    [Test]
-    public void older_target_frameworks() => new UseSlnxFiles()
-       .ForInlineSdkProject("""
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <TargetFramework>net8.0</TargetFramework>
-  </PropertyGroup>
-
-</Project>
-""")
-       .HasNoIssues();
+       .HasIssue(Issue.WRN("Proj5000", "Use a SLNX solution file instead"));
 }
--- src/DotNetProjectFile.Analyzers/Analyzers/Slnx/UseSlnxFiles.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/Slnx/UseSlnxFiles.cs
@@ -13,28 +13,9 @@ public sealed class UseSlnxFiles() : MsBuildProjectFileAnalyzer(Rule.SLNX.UseSln
     /// <inheritdoc />
     protected override void Register(ProjectFileAnalysisContext context)
     {
-        if (context.Options.AdditionalFiles.None(f => IOFile.Parse(f.Path).Extension.IsMatch(".slnx"))
-            && !Net90OrUp(context))
+        if (context.Options.AdditionalFiles.None(f => IOFile.Parse(f.Path).Extension.IsMatch(".slnx")))
         {
             context.ReportDiagnostic(Descriptor, context.File);
         }
-    }
-
-    private static bool Net90OrUp(ProjectFileAnalysisContext context)
-    {
-        foreach (var text in context.Options.AdditionalFiles)
-        {
-            if (ProjectFiles.Global.MsBuildProject(text) is { } project
-                && (project.PropertyGroups.Children<TargetFramework>().Select(tf => tf.Value).Any(Net90OrUp)
-                    || project.PropertyGroups.Children<TargetFrameworks>().Select(tf => tf.Value).SelectMany(v => v).Any(Net90OrUp)))
-            {
-                return true;
-            }
-        }
-        return false;
-
-        static bool Net90OrUp(string? str)
-            => str.Contains(TargetFramework.net9_0, StringComparison.OrdinalIgnoreCase)
-            || str.Contains(TargetFramework.net10_0, StringComparison.OrdinalIgnoreCase);
     }
 }
