2c6c6443b6d02b7fe8561fb8d55b4b6177a541b0
Corniel Nobel <corniel@gmail.com>
2025-09-30 09:50:28 +0200

    Proj0036: <None Remove=""> is valid for hiding the documentation file

--- src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/RemoveNoneWhenRedundant.cs
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/RemoveNoneWhenRedundant.cs
@@ -10,10 +10,16 @@ public sealed class RemoveNoneWhenRedundant() : MsBuildProjectFileAnalyzer(Rule.
     /// <inheritdoc />
     protected override void Register(ProjectFileAnalysisContext context)
     {
+        var documentationFile = context.File.Property<DocumentationFile>();
+
         foreach (var none in context.File.ItemGroups
-            .Children<None>(n => n.Remove.Any()))
+            .Children<None>(n => n.Remove.Any() && !Except(n)))
         {
             context.ReportDiagnostic(Descriptor, none, string.Concat(';', none.Remove));
         }
+
+        bool Except(None none)
+            => documentationFile is { } doc
+            && none.Remove.Any(remove => remove == doc.Value);
     }
 }
