94843865b584f52e58140f65f89e25789f67e386
Corniel Nobel <corniel@gmail.com>
2025-11-02 20:35:42 +0100

    Proj0812: Remove redundant packages references that are globally refecenced (#578)
    Co-authored-by: Wesley Baartman <9980136+CptWesley@users.noreply.github.com>

---
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Remove_redundant_references_for_globally_referenced.cs
@@ -0,0 +1,19 @@
+namespace Rules.MS_Build.Remove_redundant_references_for_globally_referenced;
+
+public class Reports
+{
+    [Test]
+    public void on_redundant_included() => new RemoveRedundantReferencesForGloballyReferenced()
+            .ForProject("RemoveRedundantReferencesForGloballyReferenced.cs")
+            .HasIssue(
+                Issue.WRN("Proj0812", "Remove redundant reference 'DotNetProjectFile.Analyzers'").WithSpan(07, 04, 07, 62));
+}
+
+public class Guards
+{
+    [TestCase("CompliantCSharp.cs")]
+    [TestCase("CompliantCSharpPackage.cs")]
+    public void project_files_as_additional(string project) => new RemoveRedundantReferencesForGloballyReferenced()
+        .ForProject(project)
+        .HasNoIssues();
+}
