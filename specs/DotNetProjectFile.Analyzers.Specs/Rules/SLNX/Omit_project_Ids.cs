ee22dcc24994586662c932d3390abd92034a12d7
Corniel Nobel <corniel@gmail.com>
2026-03-23 19:42:59 +0100

    ident

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/SLNX/Omit_project_Ids.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/SLNX/Omit_project_Ids.cs
@@ -5,9 +5,9 @@ public class Reports
     [Test]
     public void Non_exsiting_projects() => new Slnx.OmitProjectIds()
         .ForInlineSlnx("""
-<Solution>
-    <Project Path=".net.csproj" Id="EB70735D-B797-4E9A-A508-A98BC21C0EDD" />
-</Solution>
-""")
+            <Solution>
+              <Project Path=".net.csproj" Id="EB70735D-B797-4E9A-A508-A98BC21C0EDD" />
+            </Solution>
+            """)
         .HasIssue(Issue.WRN("Proj5005", "Remove the Project ID"));
 }
