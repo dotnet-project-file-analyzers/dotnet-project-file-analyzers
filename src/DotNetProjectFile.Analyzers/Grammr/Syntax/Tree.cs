96f373b7cae5996c6253246ddbff3e10d5b5a452
Corniel Nobel <corniel@gmail.com>
2026-04-09 21:57:47 +0200

    IDE0290: Primary constructors

--- src/DotNetProjectFile.Analyzers/Grammr/Syntax/Tree.cs
+++ src/DotNetProjectFile.Analyzers/Grammr/Syntax/Tree.cs
@@ -2,24 +2,17 @@ using Grammr.Text;
 
 namespace Grammr.Syntax;
 
-public sealed class Tree
+public sealed class Tree(Node root, TokenStream tokens, string? errorMessage)
 {
-    public Tree(Node root, TokenStream tokens, string? errorMessage)
-    {
-        Root = root;
-        Tokens = tokens;
-        ErrorMessage = errorMessage;
-    }
-
     /// <summary>Gets the root node of the syntax tree.</summary>
-    public Node Root { get; }
+    public Node Root { get; } = root;
 
     /// <summary>The token stream of the tree.</summary>
-    public TokenStream Tokens { get; }
+    public TokenStream Tokens { get; } = tokens;
 
     /// <summary>Indicates if the parsing was successful.</summary>
     public bool Success => ErrorMessage is null && Tokens.Remaining.Length == 0;
 
     /// <summary>The (optional) error message.</summary>
-    public string? ErrorMessage { get; }
+    public string? ErrorMessage { get; } = errorMessage;
 }
