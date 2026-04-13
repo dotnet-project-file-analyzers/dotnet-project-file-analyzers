0f5794cc074ffadf459ac156e356669508088cb5
Corniel Nobel <corniel@gmail.com>
2026-04-13 11:40:43 +0200

    Proj1703: MSBuild child elements should be seperated by an empty line (#715)
    Co-authored-by: Laura Kramer <laurakramer91@gmail.com>

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/ChildElementsShouldBeSeparated.cs
@@ -0,0 +1,94 @@
+using Microsoft.CodeAnalysis.Text;
+
+namespace DotNetProjectFile.Analyzers.MsBuild;
+
+/// <summary>Implements <see cref="Rule.ChildElementsShouldBeSeparated"/>.</summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class ChildElementsShouldBeSeparated()
+    : MsBuildProjectFileAnalyzer(Rule.ChildElementsShouldBeSeparated)
+{
+    /// <inheritdoc />
+    public override ImmutableArray<ProjectFileType> ApplicableTo => ProjectFileTypes.AllExceptDirectoryPackages;
+
+    /// <inheritdoc />
+    public override bool DisableOnFailingImport => false;
+
+    /// <inheritdoc />
+    protected override void Register(ProjectFileAnalysisContext context)
+    {
+        var project = context.File.Project;
+
+        List<Block> blocks =
+        [
+            new Block(0, context.File.Locations.Start, BlockType.Root),
+            .. context.File.Children.Select(NodeBlock),
+            new Block(context.File.Locations.End, context.File.Locations.End, BlockType.Root),
+        ];
+
+        blocks.AddRange(project.Element
+            .DescendantNodes()
+            .OfType<XComment>()
+            .Select(x => new XmlComment(x, context.File))
+            .Select(CommentBlock).Where(c => blocks.None(b => b.Intersect(c))));
+
+        blocks.Sort();
+
+        for (var i = 0; i < blocks.Count - 1; i++)
+        {
+            var (prev, curr) = (blocks[i], blocks[i + 1]);
+
+            var space = Space(prev.Type, curr.Type);
+            var delta = (space - (curr.Start - prev.End)) switch
+            {
+                < 0 => "Remove",
+                > 0 => "Insert",
+                _ => null,
+            };
+
+            if (delta is { })
+            {
+                var line = new LinePositionSpan(new(prev.End, 0), new(prev.End + 1, 0));
+                var location = Location.Create(project.Path.ToString(), project.Text.TextSpan(line), line);
+                context.ReportDiagnostic(Descriptor, location, delta);
+            }
+        }
+    }
+
+    private static int Space(BlockType prev, BlockType curr) => (prev, curr) switch
+    {
+        (BlockType.Import, BlockType.Import) => 1,
+        (BlockType.Comment, BlockType.Comment) => 2,
+        (BlockType.Comment, _) => 1,
+        _ => 2,
+    };
+
+    private static Block CommentBlock(XmlComment x) => new(x.Locations.Start, x.Locations.End, BlockType.Comment);
+
+    private static Block NodeBlock(Node n) => new(n.Locations.Start, n.Locations.End, n is Import ? BlockType.Import : BlockType.Block);
+
+    private enum BlockType
+    {
+        Root,
+        Comment,
+        Block,
+        Import,
+    }
+
+    private readonly record struct Block(int Start, int End, BlockType Type) : IComparable<Block>
+    {
+        public int CompareTo(Block other) => Start.CompareTo(other.Start);
+
+        public bool Intersect(Block other)
+            => other.End >= Start
+            && other.Start <= End;
+    }
+}
+
+file static class Extensions
+{
+    extension(XmlLocations loc)
+    {
+        public int Start => loc.Positions.StartElement.Start.Line;
+        public int End => loc.Positions.EndElement.End.Line;
+    }
+}
