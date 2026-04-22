8051aa5752686045dd518f90d65425ef943d9589
Corniel Nobel <corniel@gmail.com>
2026-04-23 01:39:58 +0200

    Remove

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Define_single_target_framework.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Define_single_target_framework.cs
@@ -3,21 +3,31 @@ namespace Rules.MS_Build.Define_single_target_framework;
 public class Reports
 {
     [Test]
-    public void defined_in_target_frameworks()
-        => new DefineSingleTargetFramework()
-        .ForProject("TargetFrameworksSingle.cs")
+    public void defined_in_target_frameworks() => new DefineSingleTargetFramework()
+        .ForInlineCsproj("""
+        <Project Sdk="Microsoft.NET.Sdk">
+
+          <PropertyGroup>
+            <TargetFrameworks>net10.0</TargetFrameworks>
+          </PropertyGroup>
+
+        </Project>
+        """)
         .HasIssues(
             Issue.WRN("Proj0009", "Use the <TargetFramework> node instead").WithSpan(3, 4, 3, 48));
 }
 
 public class Guards
 {
-    [TestCase("CompliantCSharp.cs")]
-    [TestCase("CompliantCSharpPackage.cs")]
-    [TestCase("TargetFrameworksMultiple.cs")]
-    [TestCase("TargetFrameworksSingleWithBase.cs")]
-    public void Projects_with_analyzers(string project)
-         => new DefineSingleTargetFramework()
-        .ForProject(project)
+    [Test]
+    public void Projects_with_analyzers() => new DefineSingleTargetFramework().ForInlineCsproj("""
+        <Project Sdk="Microsoft.NET.Sdk">
+
+          <PropertyGroup>
+            <TargetFramework>net10.0</TargetFramework>
+          </PropertyGroup>
+
+        </Project>
+        """)
         .HasNoIssues();
 }
