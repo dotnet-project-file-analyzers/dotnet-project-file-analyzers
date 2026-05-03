e70731965add53212bbc52d2e387725f84c354c2
Corniel Nobel <corniel@gmail.com>
2026-05-04 01:49:33 +0200

    Update specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Add_additional_files.cs
    Co-authored-by: Wesley Baartman <9980136+CptWesley@users.noreply.github.com>

--- specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Add_additional_files.cs
+++ specs/DotNetProjectFile.Analyzers.Specs/Rules/MS_Build/Add_additional_files.cs
@@ -17,7 +17,7 @@ public class Guards
         .ForProject(project)
         .HasNoIssues();
 
-    [Test, Ignore("Should be fixed with mergining the SDK ")]
+    [Test, Ignore("Should be fixed with merging the SDK ")]
     public void Directory_Build_props_not_being_added() => new AddAdditionalFile()
         .ForProject("WithDirectoryBuildProps.cs")
         .HasNoIssues();
