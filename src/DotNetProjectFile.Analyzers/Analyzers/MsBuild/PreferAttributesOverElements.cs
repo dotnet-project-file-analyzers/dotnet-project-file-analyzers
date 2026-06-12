606b58eddde51de966eb3992e40fddab3dde9e29
Corniel Nobel <corniel@gmail.com>
2026-06-12 11:55:06 +0200

    Proj0052: Prefer attributes over elements (#823)
    Co-authored-by: Laura Kramer <laurakramer91@gmail.com>

---
+++ src/DotNetProjectFile.Analyzers/Analyzers/MsBuild/PreferAttributesOverElements.cs
@@ -0,0 +1,33 @@
+namespace DotNetProjectFile.Analyzers.MsBuild;
+
+/// <summary>Implements <see cref="Rule.PreferAttributesOverElements"/>.</summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class PreferAttributesOverElements() : MsBuildProjectFileAnalyzer(Rule.PreferAttributesOverElements)
+{
+    /// <inheritdoc />
+    public override bool DisableOnFailingImport => false;
+
+    /// <inheritdoc />
+    protected override void Register(ProjectFileAnalysisContext context) => Walk(context.File, context);
+
+    private void Walk(Node node, ProjectFileAnalysisContext context)
+    {
+        if (node.Children.Any())
+        {
+            foreach (var child in node.Children.Where(Applicable))
+                Walk(child, context);
+        }
+        else if (CanBeAttribute(node))
+        {
+            context.ReportDiagnostic(Descriptor, node, node.LocalName);
+        }
+    }
+
+    private static bool Applicable(Node node) => node
+        is not PropertyGroup
+        and not ItemDefinitionGroup;
+
+    private static bool CanBeAttribute(Node node)
+        => node.Element.Attributes().None()
+        && !node.Element.Value.Trim().Contains('\n');
+}
