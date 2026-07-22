7ceef27c68394072182b2d8cc1f821794efd75b3
Corniel Nobel <corniel@gmail.com>
2026-07-22 14:14:35 +0200

    Use inline construction

--- specs/Specs/Rules/EditorConfig/Use_equals_for_assignment.cs
+++ specs/Specs/Rules/EditorConfig/Use_equals_for_assignment.cs
@@ -6,10 +6,15 @@ public class Reports
 {
     [Test]
     public void colon_signs() => new UseEqualsAssign()
-        .ForProject("InvalidEditorConfigHeaders.cs")
-        .HasIssues(
-            Issue.WRN("Proj4051", "Use = instead").WithSpan(13, 12, 13, 13),
-            Issue.WRN("Proj4051", "Use = instead").WithSpan(14, 11, 14, 12));
+        .ForInlineEditorconfig("""
+        root = true
+
+        [*]
+        end_of_line: crlf
+        
+        """)
+        .HasIssue(
+            Issue.WRN("Proj4051", "Use '=' instead").WithSpan(03, 11, 03, 12));
 }
 
 public class Guards
