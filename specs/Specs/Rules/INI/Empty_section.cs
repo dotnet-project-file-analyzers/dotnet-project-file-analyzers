de5837a5d9b22873e0317d3c16cbf9c193d23a46
Corniel Nobel <corniel@gmail.com>
2026-07-22 13:50:56 +0200

    Use inline INI files in tests

--- specs/Specs/Rules/INI/Empty_section.cs
+++ specs/Specs/Rules/INI/Empty_section.cs
@@ -6,7 +6,21 @@ public class Reports
 {
     [Test]
     public void empty_sections() => new EmptySection()
-        .ForProject("IniFileWithEmptySection.cs")
+        .ForInlineEditorconfig("""
+        root = true
+
+        [*]
+        end_of_line = crlf
+
+        # Code files
+        [*.{cs,json,cshtml,ts}]
+        indent_style = space
+
+        [empty.*]
+        # Roslyn Analyzers
+        [*.cs]
+        vsspell_section_id = main
+        """)
         .HasIssue(Issue.WRN("Proj4010", "Section [empty.*] is empty").WithSpan(09, 00, 10, 00));
 }
 
