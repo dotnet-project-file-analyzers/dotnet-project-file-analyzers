f972260855eacde3e7db102ee51b2f606440fc77
Corniel Nobel <corniel@gmail.com>
2026-07-25 16:59:46 +0200

    Define severity

---
+++ src/DotNetProjectFile.Analyzers/GlobalConfig/DiagnosticSeverity.cs
@@ -0,0 +1,34 @@
+namespace DotNetProjectFile.GlobalConfig;
+
+public enum DiagnosticSeverity
+{
+
+    /// <summary>
+    /// Suppresses the diagnostic completely; it does not run, nor does it offer fixes.
+    /// </summary>
+    none = 0,
+
+    /// <summary>
+    /// The diagnostic is invisible to the user in the UI, but still triggers
+    /// the IDE to offer associated light-bulb code fixes.
+    /// </summary>
+    silent,
+
+    /// <summary>
+    /// olations appear as messages/gray dots in the Error List and do not fail
+    /// the build.
+    /// </summary>
+    suggestion,
+
+    /// <summary>
+    /// Violations appear in the Error List as a green squiggle but do not
+    /// fail the build.
+    /// </summary>
+    warning,
+
+    /// <summary>
+    /// Violations appear in the Error List, cause command-line builds to fail,
+    /// and are marked with a red squiggle.
+    /// </summary>
+    error,
+}
