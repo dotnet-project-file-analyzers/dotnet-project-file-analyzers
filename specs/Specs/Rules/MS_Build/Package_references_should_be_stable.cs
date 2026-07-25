531a0e0004a62866a4724359e6d115ab6dd34774
Corniel Nobel <corniel@gmail.com>
2026-07-25 18:47:46 +0200

    Report on unstable versions of development dependencies too (#877)

--- specs/Specs/Rules/MS_Build/Package_references_should_be_stable.cs
+++ specs/Specs/Rules/MS_Build/Package_references_should_be_stable.cs
@@ -3,20 +3,58 @@ namespace Rules.MS_Build.Package_references_should_be_stable;
 public class Reports
 {
     [Test]
-    public void unstable_versions()
-        => new PackageReferencesShouldBeStable()
-        .ForProject("UnstablePackageReferences.cs")
-        .HasIssues(
-            Issue.WRN("Proj1101", "Use a stable version of 'System.IO.Hashing', instead of '9.0.0-preview.7.24405.7'").WithSpan(08, 04, 08, 86).WithPath("UnstablePackageReferences.csproj"),
-            Issue.WRN("Proj1101", "Use a stable version of 'System.Text.Json', instead of '*-*'" /*.................*/).WithSpan(09, 04, 09, 65).WithPath("UnstablePackageReferences.csproj"));
+    public void unstable_versions() => new PackageReferencesShouldBeStable()
+        .ForInlineCsproj("""
+        <Project Sdk="Microsoft.NET.Sdk">
+
+          <PropertyGroup>
+            <TargetFramework>net10.0</TargetFramework>
+          </PropertyGroup>
+
+          <ItemGroup>
+            <PackageReference Include="Qowaiv" Version="8.0.0" />
+            <PackageReference Include="System.IO.Hashing" Version="9.0.0-preview.7.24405.7" />
+          </ItemGroup>
+
+        </Project>
+        """)
+        .HasIssue(Issue
+            .WRN("Proj1101", "Use a stable version of 'System.IO.Hashing', instead of '9.0.0-preview.7.24405.7'")
+            .WithSpan(08, 04, 08, 86));
 
     [Test]
-    public void unstable_versions_via_CPM()
-        => new PackageReferencesShouldBeStable()
+    public void unstable_versions_via_CPM() => new PackageReferencesShouldBeStable()
         .ForProject("UnstableVersionsCPM.cs")
         .HasIssues(
-            Issue.WRN("Proj1101", "Use a stable version of 'System.IO.Hashing', instead of '9.0.0-rc.2.24473.5'").WithSpan(09, 04, 09, 88).WithPath("UnstableVersionsCPM.csproj"),
-            Issue.WRN("Proj1101", "Use a stable version of 'Warpstone', instead of '2.0.0-preview2'").WithSpan(10, 04, 10, 67).WithPath("Directory.Packages.props"));
+            Issue.WRN("Proj1101", "Use a stable version of 'StyleCop.Analyzers', instead of '*-*'")
+                .WithSpan(08, 04, 08, 65).WithPath("Directory.Packages.props"),
+            Issue.WRN("Proj1101", "Use a stable version of 'System.IO.Hashing', instead of '9.0.0-preview.7.24405.7'")
+                .WithSpan(09, 04, 09, 84).WithPath("Directory.Packages.props"),
+            Issue.WRN("Proj1101", "Use a stable version of 'System.IO.Hashing', instead of '9.0.0-rc.2.24473.5'")
+                .WithSpan(09, 04, 09, 88).WithPath("UnstableVersionsCPM.csproj"),
+            Issue.WRN("Proj1101", "Use a stable version of 'Warpstone', instead of '2.0.0-preview2'")
+                .WithSpan(10, 04, 10, 67).WithPath("Directory.Packages.props"));
+
+
+    [Test]
+    public void unstable_global_package_references() => new PackageReferencesShouldBeStable()
+        .ForInlineCsproj(
+        """
+        <Project Sdk="Microsoft.NET.Sdk">
+        
+          <PropertyGroup>
+            <TargetFramework>net10.0</TargetFramework>
+          </PropertyGroup>
+
+          <ItemGroup>
+            <GlobalPackageReference Include="StyleCop.Analyzers" Version="1.2.0-beta.556" />
+          </ItemGroup>
+        
+        </Project>
+        """)
+        .HasIssue(Issue
+            .WRN("Proj1101", "Use a stable version of 'StyleCop.Analyzers', instead of '1.2.0-beta.556'")
+            .WithSpan(07, 04, 07, 84));
 }
 
 public class Guards
--- src/DotNetProjectFile.Analyzers/MsBuild/PackageReference.cs
+++ src/DotNetProjectFile.Analyzers/MsBuild/PackageReference.cs
@@ -12,4 +12,39 @@ public sealed class PackageReference(XElement element, Node parent, MsBuildProje
     public string? PrivateAssets => Attribute() ?? Child();
 
     public override PackageVersionInfo Info => new(IncludeOrUpdate, VersionOrVersionOverride);
+
+    public override (Node Node, string Version)? ResolveVersionVerbose(bool cpmEnabled) => cpmEnabled switch
+    {
+        true when VersionOverride is { Length: > 0 } fixedVersion
+            => (this, fixedVersion),
+
+        true when WalkVersionOverrides() is { } versionOverride
+            => versionOverride,
+
+        true when WalkPackageVersions() is { } packageVersion
+            => packageVersion,
+
+        _ when Version?.Length > 0
+            => (this, Version),
+
+        _ => null,
+    };
+
+    private (Node Node, string Version)? WalkVersionOverrides()
+        => Project
+            .WalkBackward()
+            .OfType<PackageReference>()
+            .FirstOrDefault(p => p.IncludeOrUpdate == IncludeOrUpdate)
+        is { VersionOverride.Length: > 0 } node
+        ? (node, node.VersionOverride)
+        : null;
+
+    private (Node Node, string Version)? WalkPackageVersions()
+        => Project
+            .WalkBackward()
+            .OfType<PackageVersion>()
+            .FirstOrDefault(v => v.Include == IncludeOrUpdate)
+        is { Version.Length: > 0 } node
+        ? (node, node.Version)
+        : null;
 }
