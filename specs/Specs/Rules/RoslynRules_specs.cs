0430dc0e2531b592e109e664cb36f8f974a6098e
Corniel Nobel <corniel@gmail.com>
2026-09-07 13:27:05 +0200

    Add Obsolete rules that or disabled by default

--- specs/Specs/Rules/RoslynRules_specs.cs
+++ specs/Specs/Rules/RoslynRules_specs.cs
@@ -27,4 +27,16 @@ public class Sets_match_DiagnosticCollection_for
 #endif
         RoslynRules.Dropped.Should().BeSameSet(ids);
     }
+
+    [Test]
+    public void Obsolete()
+    {
+        var catalog = DotNetProjectFile.RuleCatalog.DiagnosticCollection.Embedded();
+        var rules = catalog.Rules.Where(r => r is { Obsolete.Length: > 0, IsEnabledByDefault: false } ).ToArray();
+        var ids = rules.Select(r => r.Id.ToString()).ToHashSet();
+#if DEBUG
+        Console.WriteLine(string.Join(';', ids.Order()));
+#endif
+        RoslynRules.Obsolete.Should().BeSameSet(ids);
+    }
 }
