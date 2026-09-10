9a53d0127dadb1c3ccd8c7c8d3e9f98d9bbeec46
Corniel Nobel <corniel@gmail.com>
2026-09-10 09:37:23 +0200

    Update dependencies

--- specs/Specs/Rules/GlobalConfig/Avoid_enabling_deprecated_rules.cs
+++ specs/Specs/Rules/GlobalConfig/Avoid_enabling_deprecated_rules.cs
@@ -6,19 +6,19 @@ public class Reports
 {
     [Test]
     public void enabled_deprecated_rules() => new AvoidEnablingDeprecatedRules().ForInlineGlobalconfig("""
-        dotnet_diagnostic.AV0000.severity = warning
-        dotnet_diagnostic.Proj1000.severity = silent
+        dotnet_diagnostic.S2228.severity = warning
+        dotnet_diagnostic.RCS1012.severity = silent
         """)
         .HasIssues(
-            Issue.WRN("Proj1010", "Rule AV0000 is deprecated and should not be enabled").WithSpan(00, 00, 00, 34),
-            Issue.WRN("Proj1010", "Rule Proj1000 is deprecated and should not be enabled").WithSpan(01, 00, 01, 36));
+            Issue.WRN("Proj1010", "Rule S2228 is deprecated and should not be enabled").WithSpan(00, 00, 00, 33),
+            Issue.WRN("Proj1010", "Rule RCS1012 is deprecated and should not be enabled").WithSpan(01, 00, 01, 35));
 }
 
 public class Guards
 {
     [Test]
     public void disabled_deprecated_rules() => new AvoidEnablingDeprecatedRules().ForInlineGlobalconfig("""
-        dotnet_diagnostic.AV0000.severity = none
+        dotnet_diagnostic.S2228.severity = none
         """)
         .HasNoIssues();
 
